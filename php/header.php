cc76013202cae8bdbbbb6b87b706cf77d3558b3e
Kushal Harsora <kushalharsora13@gmail.com>
2024-04-30 13:04:32 +0530

    Navbar done

--- php/header.php
+++ php/header.php
@@ -1,11 +1,11 @@
 <header id=" header" class="header fixed-top" data-scrollto-offset="0">
     <div class="container-fluid d-flex align-items-center justify-content-between" style="position:static;background-color:white; margin-top:-20px">
-        <img src="assets/img/logo.png" alt="Logo" style="height: 70px; max-width: 100%;">
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fseeklogo.com%2Fimages%2FS%2Fsomaiya-vidyavihar-university-logo-13C717072D-seeklogo.com.png&f=1&nofb=1&ipt=c226e6506b51de197b401463b5efde9e361780c6c05a08f7b59e57ca7a0758e9&ipo=images" alt="Logo" style="height: 80px; max-width: 100%; margin: 10px 0;">
         <a href="index.php" class="logo d-flex align-items-center scrollto me-auto me-lg-0">
 
         </a>
 
-        <nav id="navbar" class="navbar" style="justify-content:center;margin-left:-550px">
+        <nav id="navbar" class="navbar" style="display: flex;justify-content:center;margin-left:0px;margin-bottom:0px;">
             <ul>
 
                 <li><a class="nav-link scrollto" href="index.php">Home</a></li>
